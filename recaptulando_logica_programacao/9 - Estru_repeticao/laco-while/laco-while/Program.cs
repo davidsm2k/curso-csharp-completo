05b371416cfb6d54325352212b3cd6925e27c6f0
David Santos <69879401+davidsm2k@users.noreply.github.com>
2022-03-16 22:08:04 -0300

    Add aulas sobre laco de repeticao for e while

---
+++ recaptulando_logica_programacao/9 - Estru_repeticao/laco-while/laco-while/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace laco_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+
+            do
+            {
+                Console.WriteLine("Digite um numero: ");
+                numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (numero !>= 0)
+                {             
+                    double calculo = Math.Sqrt(numero);
+                    Console.WriteLine($"Raiz quadrada de {numero} é {calculo.ToString("F3", CultureInfo.InvariantCulture)}");
+                }
+                else
+                {
+                    Console.WriteLine("Número negativo!");
+                }
+            } while (numero >= 0);
+
+        }
+    }
+}
---
+++ recaptulando_logica_programacao/9 - Estru_repeticao/laco_for/laco_for/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace laco_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int digitos, soma = 0;
+
+            Console.Write("Quantos numeros inteiros voce vai digitar? ");
+            digitos = int.Parse(Console.ReadLine());
+
+            for (int x = 1; digitos >= x; x++)
+            {
+                Console.Write($"Digite o valor {x}: ");
+                soma += int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Soma = {soma}");
+
+        }
+    }
+}
