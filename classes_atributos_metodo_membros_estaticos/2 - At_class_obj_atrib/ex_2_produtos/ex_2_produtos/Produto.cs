5d1de5c07bcb4b3f368f475a367d443de3024b5b
David Santos <69879401+davidsm2k@users.noreply.github.com>
2022-03-21 22:51:08 -0300

    Add exemplo 2 Produtos

---
+++ classes_atributos_metodo_membros_estaticos/2 - At_class_obj_atrib/ex_2_produtos/ex_2_produtos/Produto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex_2_produtos
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return this.Quantidade * this.Preco;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            this.Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            this.Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Nome}, " +
+                   $"$ {this.Preco.ToString("F2", CultureInfo.InvariantCulture)}, " +
+                   $"{this.Quantidade} unidades, " +
+                   $"Total: {this.ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+        /*
+        public void ListaProdutos(Produto[] Lista)
+        {
+            Console.WriteLine("Lista de produtos: ");
+            for (int i = 0; i < Lista.Length; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Nome: {Lista[i].Nome}");
+                Console.WriteLine($"Preco: {Lista[i].Preco}");
+                Console.WriteLine($"Quantidade no estoque: {Lista[i].Quantidade}");
+                Console.WriteLine("--------------------------------------");
+            }
+        }
+        */
+    }
+}
---
+++ classes_atributos_metodo_membros_estaticos/2 - At_class_obj_atrib/ex_2_produtos/ex_2_produtos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex_2_produtos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+
+            Console.WriteLine("** Entre com os dados do produto **");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preco: ");
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Dados do produto: \n \t {produto}");
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine($"Dados atualizados: \n \t {produto}");
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removidos ao estoque: ");
+            produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine($"Dados atualizados: \n \t {produto}");
+
+        }
+    }
+} 
